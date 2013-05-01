ad7cd94995759cc002424ecde15d9162ccf088ab
HD <hdvision16@gmail.com>
2013-05-01 15:24:00 -0500

    minor changes

--- acclaim/make-bootimg.sh
+++ acclaim/make-bootimg.sh
@@ -3,14 +3,14 @@
 #For internal
 #cp $CM_ROOT/device/bn/acclaim/boot/flashing_boot_emmc.img $TARGET_DIR
 
-mkbootimg --kernel zImage --cmdline 'console=/dev/null' --base 0x80080000 --pagesize 4096 --ramdisk ramdisk.cpio.gz --output boot_new.img
+mkbootimg --kernel zImage --cmdline 'console=/dev/null' --base 0x80080000 --pagesize 4096 --ramdisk ramdisk.cpio.gz --output boot.img
 
 #For internal
 #cat flashing_boot_emmc.img boot_new.img > boot.img
-mv boot_new.img boot.img
+#mv boot_new.img boot.img
 
-if [ -e boot_new.img ]; then
-	rm boot_new.img
-fi
+#if [ -e boot_new.img ]; then
+#	rm boot_new.img
+#fi
 
 exit $?
--- acclaim/make-ramdisk.sh
+++ acclaim/make-ramdisk.sh
@@ -7,9 +7,14 @@ cd $TARGET_DIR/ramdisk
 
 # make sure all directories are created because git doesn't save empty directories
 mkdir -p data dev proc sbin sys system
-chmod 750 init*
+
 chmod 750 sbin/adbd
+chmod 750 sbin/gdisk
+chmod 750 sbin/genptable
+chmod 750 sbin/mkdosfs
+chmod 750 sbin/sgdisk
 chmod 644 default.prop
 chmod 640 fstab.$DEVICE
+chmod 750 init*
 chmod 644 ueventd*
 find . | cpio -o -H newc | gzip > ../ramdisk.cpio.gz
