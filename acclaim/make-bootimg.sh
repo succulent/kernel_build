4f1036e39f741067f6439fd0ea5d8a624203b648
HD <hdvision16@gmail.com>
2013-05-01 11:22:56 -0500

    acclaim: sd boot

--- acclaim/make-bootimg.sh
+++ acclaim/make-bootimg.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cp $CM_ROOT/device/bn/acclaim/boot/flashing_boot.img $TARGET_DIR
-cp $CM_ROOT/device/bn/acclaim/boot/flashing_boot_emmc.img $TARGET_DIR
+#For internal
+#cp $CM_ROOT/device/bn/acclaim/boot/flashing_boot_emmc.img $TARGET_DIR
 
 mkbootimg --kernel zImage --cmdline 'console=/dev/null' --base 0x80080000 --pagesize 4096 --ramdisk ramdisk.cpio.gz --output boot_new.img
 
-cat flashing_boot.img boot_new.img > boot.img
+#For internal
+#cat flashing_boot_emmc.img boot_new.img > boot.img
+mv boot_new.img boot.img
 rm boot_new.img
 exit $?
