3a702f1a2e720d50e56b686f8f0f33e21de5d635
HD <hdvision16@gmail.com>
2013-05-02 12:45:52 -0500

    acclaim: make-bootimg.sh, removed pagesize bc it's not needed

--- acclaim/make-bootimg.sh
+++ acclaim/make-bootimg.sh
@@ -3,7 +3,7 @@
 #For internal
 #cp $CM_ROOT/device/bn/acclaim/boot/flashing_boot_emmc.img $TARGET_DIR
 
-mkbootimg --kernel zImage --cmdline 'console=/dev/null' --base 0x80080000 --pagesize 4096 --ramdisk ramdisk.cpio.gz --output boot.img
+mkbootimg --kernel zImage --cmdline 'console=/dev/null' --base 0x80080000 --ramdisk ramdisk-new.gz --output boot.img
 
 #For internal
 #cat flashing_boot_emmc.img boot_new.img > boot.img
